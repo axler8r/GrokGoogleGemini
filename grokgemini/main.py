5e773c699f21820a682fe4b5f09a6d5dbf70394e
AxlER8R <axl@axler8r.io>
2024-10-12 13:11:05 +1300

    style: clarify `clargs`

--- grokgemini/main.py
+++ grokgemini/main.py
@@ -15,19 +15,26 @@ __version__ = "0.1.0"
 # fmt: off
 def _parse_arguments() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="Generate content using Google Gemini API")
-    parser.add_argument("-V", "--version", action="version", version=f"%(prog)s {__version__}")
-    parser.add_argument("-s", "--system-instruction", type=argparse.FileType("r"), help="File containing the system instructions")
-    parser.add_argument("-m", "--model", type=str, help="Model to use for generation", default="gemini-1.5-flash")
-    parser.add_argument("-o", "--output", type=str, help="Output file name")
-    parser.add_argument("-v", "--verbose", action="count", help="Enable verbose logging", default=0)
 
+    # information arguments
+    parser.add_argument("--version", action="version", version=f"%(prog)s {__version__}")
+    
+    # bahvriour arguments
+    parser.add_argument("--system-instruction", type=argparse.FileType("r"), help="File containing the system instructions")
+    parser.add_argument("--model", type=str, help="Model to use for generation", default="gemini-1.5-flash")
+    parser.add_argument("--output", type=str, help="Output file name")
     parser.add_argument("--stream", action="store_true", help="Stream the output")
+    # parser.add_argument("--multi-modal", action="store_true", help="Enable multi-modal generation")
+    # parser.add_argument("--configuration", type=argparse.FileType("r"), help="Configuration file")
+    parser.add_argument("-V", "--verbose", action="count", help="Enable verbose logging", default=0)
+
+    # task arguments
     group: argparse._MutuallyExclusiveGroup = parser.add_mutually_exclusive_group()
-    group.add_argument("-l", "--list-models", action="store_true", help="List available models")
+    group.add_argument("--list-models", action="store_true", help="List available models")
     group.add_argument("-t", "--generate-text", action="store_true", help="Generate text content")
-    group.add_argument("-i", "--generate-image", action="store_true", help="Generate image")
     group.add_argument("-d", "--describe-image", type=argparse.FileType("rb"), help="Describe image (valid formats: jpeg, png)")
 
+    # positional arguments
     parser.add_argument("instruction", nargs="*", help="Input for the generation")
 
     return parser.parse_args()
