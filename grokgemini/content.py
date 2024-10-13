4b9ebb486d2f9965037249e46f2f30c09d2100ed
AxlER8R <axl@axler8r.io>
2024-10-13 21:23:44 +1300

    doc: add module documentation

--- grokgemini/content.py
+++ grokgemini/content.py
@@ -1,3 +1,19 @@
+"""
+This module provides functionality to generate content using Google's generative
+AI models.
+
+Functions:
+    generate(
+        instruction: str,
+        model: str,
+        api_key: str, stream: bool = False,
+        system_instruction: Optional[str] = None
+    ) -> GenerateContentResponse:
+
+    describe() -> None:
+        Raises a NotImplementedError indicating that image generation is not yet implemented.
+"""
+
 from typing import Optional
 
 import google.generativeai as genai
@@ -11,6 +27,23 @@ def generate(
     stream: bool = False,
     system_instruction: Optional[str] = None,
 ) -> GenerateContentResponse:
+    """
+    Generates content based on the provided instruction using a specified generative model.
+
+    Args:
+        instruction (str): The instruction or prompt to generate content from.
+        model (str): The name of the generative model to use.
+        api_key (str): The API key for authentication with the generative model service.
+        stream (bool, optional): Whether to stream the response. Defaults to False.
+        system_instruction (Optional[str], optional): Additional system instructions for the model. Defaults to None.
+
+    Returns:
+        GenerateContentResponse: The generated content response from the model.
+
+    Raises:
+        genai.APIError: If an API error occurs during content generation.
+        Exception: If an unexpected error occurs.
+    """
     try:
         genai.configure(api_key=api_key)
         model_instance = genai.GenerativeModel(
